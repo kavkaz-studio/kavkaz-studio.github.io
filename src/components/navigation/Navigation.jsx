5bd712ec7b64a98e86516e1921d6f98f72f29a6b
kavkaz-studio <karenhovhannisyan197@gmail.com>
2022-09-25 19:35:44 +0300

    fix

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -37,7 +37,7 @@ const Navigation = ({ home }) => {
                             {links.map(({ name, to, text }, i) => {
                                 if (name === "home") {
                                     return <Link key={i} to={to}>
-                                        <img className="home-icon" src={`https://img.icons8.com/material-outlined/30/${location === "/kavkaz-studio" ? "e6e3d0" : "cfbeac"}/home--v2.svg`} alt="homeIcon" />
+                                        <img className="home-icon" src={`https://img.icons8.com/material-outlined/30/${location === "/" ? "e6e3d0" : "cfbeac"}/home--v2.svg`} alt="homeIcon" />
                                     </Link>
                                 }
                                 return <Link key={i} to={to} style={location.replace("/kavkaz-studio/", "") === name ? { color: "#e6e3d0" } : null}>{text}</Link>
@@ -52,7 +52,7 @@ const Navigation = ({ home }) => {
                     {links.map(({ name, to, text }, i) => {
                         if (name === "home") {
                             return <Link key={i} to={to}>
-                                <img className="home-icon" src={`https://img.icons8.com/material-outlined/30/${location === "/kavkaz-studio/" ? "e6e3d0" : "cfbeac"}/home--v2.svg`} alt="homeIcon" />
+                                <img className="home-icon" src={`https://img.icons8.com/material-outlined/30/${location === "/" ? "e6e3d0" : "cfbeac"}/home--v2.svg`} alt="homeIcon" />
                             </Link>
                         }
                         return <Link key={i} to={to} style={location.replace("/", "") === name ? { color: "#e6e3d0" } : null}>{text}</Link>
