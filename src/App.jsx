69dfa8c22e4685a08ab1e75b742d61228fb13787
kavkaz-studio <karenhovhannisyan197@gmail.com>
2022-09-25 23:51:11 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
     const navigate = useNavigate()
     
     useEffect(() => {
-        navigate("/kavkaz-studio")
+        navigate("/")
         document.addEventListener("beforeunload", () => {
             navigate("/")
         })
