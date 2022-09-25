5e2289c653cd1b6c7a65f338908fb5912d71a1cb
ashmuradyann <ashot.muradyan16@gmail.com>
2022-09-25 13:37:53 +0300

    fix

--- src/components/partners/Partners.jsx
+++ src/components/partners/Partners.jsx
@@ -11,7 +11,7 @@ const Partners = ({ partners }) => {
           {partners.first.whatsApp && <p>WhatsApp: <a href={`https://wa.me/${partners.first.whatsApp.replace("+", "")}`} target="_blank">{partners.first.whatsApp}</a></p>}
           {partners.first.mail && <p>Mail: <a href={`mailto:${partners.first.mail}`} target="_blank">{partners.first.mail}</a></p>}
           {partners.first.vk && <p>ВКонтакте: <a href={partners.first.vk} target="_blank">{partners.first.vk}</a></p>}
-          {partners.first.phone && <p>Тел: {partners.first.phone.map((el, i) => <a key={i} href={`tel:${el}`}>{el}</a>)}</p>}
+          {partners.first.phone.lenght !== 0 && <p>Тел: {partners.first.phone.map((el, i) => <a key={i} href={`tel:${el}`}>{el}</a>)}</p>}
         </div>
         <div>
           {partners.second.title && <h1>{partners.second.title}</h1>}
@@ -20,7 +20,7 @@ const Partners = ({ partners }) => {
           {partners.second.whatsApp && <p>WhatsApp: <a href={`https://wa.me/${partners.second.whatsApp.replace("+", "")}`} target="_blank">{partners.second.whatsApp}</a></p>}
           {partners.second.mail && <p>Mail: <a href={`mailto:${partners.second.mail}`} target="_blank">{partners.second.mail}</a></p>}
           {partners.second.vk && <p>ВКонтакте: <a href={partners.second.vk} target="_blank">{partners.second.vk}</a></p>}
-          {partners.second.phone && <p>Тел: {partners.second.phone.map((el, i) => <a key={i} href={`tel:${el}`}>{el}</a>)}</p>}
+          {partners.second.phone.lenght !== 0 && <p>Тел: {partners.second.phone.map((el, i) => <a key={i} href={`tel:${el}`}>{el}</a>)}</p>}
         </div>
       </div>
     </div>
